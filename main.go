db0539d1e95aae6c7cb351589583e2368722be51
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-07-27 17:38:29 +0545

    non-ideal way: execute go routines

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -21,6 +22,9 @@ func main() {
 
 	**/
 
+	// non-ideal way to wait complete for all goroutines to complete
+	start := time.Now()
+	fmt.Println("StartTime:", start)
 	// gorun time to launch goroutine to run producer function with paramter 1
 	go producer(1)
 
@@ -30,22 +34,29 @@ func main() {
 	//  pitfall :  but before any of this function runs, main function returns
 	// immediately(ends main function and all goroutines get killed)
 
-	// solve:
+	// give goroutines time to complete work
 	time.Sleep(1 * time.Second)
-
-	// creating goroutines from anonymous function
-	go func() {
-		for i := 0; i < 5; i++ {
-			fmt.Printf("Foo() - message # %v\n", i)
-		}
-		go producer(3)
-	}()
+	elaspe := time.Now().Sub(start)
+	fmt.Printf("None-ideal wait took%v\n", elaspe)
 
 }
 
-//
 func producer(id int) {
-	for i := 0; i < 5; i++ {
-		fmt.Printf("Producer # %v - message # %v\n", id, i)
-	}
+
+	// n is random int between 1 to 1000 inclusive
+
+	n := (rand.Intn(1000) % 1000) + 1
+	d := time.Duration(n) * time.Millisecond
+	time.Sleep(d)
+	fmt.Printf("Producer # %v ran for %v\n±", id, d)
 }
+
+/*
+Output:
+
+StartTime: 2021-07-27 17:34:07.848502 +0545 +0545 m=+0.000098418
+Producer # 1 ran for 82ms
+±Producer # 2 ran for 888ms
+±None-ideal wait took1.004336208s
+
+*/
