c6fac0bd8a2a8920b6fd718539e3c9160312b643
Denes-cilwal <dinesh.silwal@wesionary.team>
2022-01-19 12:24:01 +0545

    feat:passing value across context

---
+++ context-demo/logger.go/main.go
@@ -0,0 +1,36 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+// takes context
+func Println(ctx context.Context, msg string) {
+	// get value from the context with key
+	v, ok := ctx.Value("requestID").(int64)
+	fmt.Println(v)
+	if !ok {
+		fmt.Println("request id not found")
+		return
+	}
+	log.Printf("%v -> %s", v, msg)
+}
+
+func Decorator(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		reqID := rand.Int63()
+		// get context from request
+		ctx := r.Context()
+
+		// set context with key in request context
+		ctx = context.WithValue(ctx, "requestID", reqID)
+
+		// WithContext returns a shallow copy of r with its context changed to ctx.
+		// The provided ctx must be non-nil.
+		f(w, r.WithContext(ctx))
+	}
+}
--- context-demo/server/main.go
+++ context-demo/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context-demo/context-demo/logger.go"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,9 +10,13 @@ import (
 
 func main() {
 
-	http.HandleFunc("/", handler)
+	// http.HandleFunc("/", handler)
+	http.HandleFunc("/", logger.Decorator(handler))
 	log.Panic(http.ListenAndServe("127.0.0.1:3000", nil))
 }
+
+// every time when we get | recieve request in handler we need to make a request id and then value of
+// request id in the context.value
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
@@ -20,7 +25,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	case <-time.After(2 * time.Second):
 		fmt.Fprintln(w, "Hello")
 	case <-ctx.Done():
-		log.Println(ctx.Err().Error())
+		// log.Println(ctx.Err().Error())
+		logger.Println(ctx, ctx.Err().Error())
 		http.Error(w, ctx.Err().Error(), http.StatusInternalServerError)
 	}
 	fmt.Println(w, "hello")
